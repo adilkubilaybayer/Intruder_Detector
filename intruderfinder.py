b9f0a9ab79a7a01e151684c959e7f29b2e842619
adilkubilaybayer <101289501+adilkubilaybayer@users.noreply.github.com>
2022-04-01 00:17:27 +0200

    Update intruderfinder.py
    File path system is fixed. Now it writes to the location of the .fy file.

--- intruderfinder.py
+++ intruderfinder.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 
@@ -15,6 +16,10 @@ def get_frame(cap):
 ##    (thresh, blackAndWhiteImage) = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 ##we can make canny with this(canny but have errors)
     return gray
+#takes file name as string input and adds .jpg
+raw=str(input("How should i name your file"))
+raw=raw+".jpg" #adding .jpg
+path = os.getcwd() #path of our program
 if __name__=='__main__':
 
     cap = cv2.VideoCapture(0)
@@ -24,43 +29,19 @@ if __name__=='__main__':
     cur_frame = get_frame(cap)
 
     next_frame = get_frame(cap)
-    #cv2.imwrite(r'C:/Users/aselb/Desktop/samplenew.png', next_frame)
-    c=0
-    list1=[0]
 while True:
-    c=c+1
     a=frame_difference(prev_frame,cur_frame, next_frame)
-    cv2.imshow('Object Movement',a)
+#    cv2.imshow('Object Movement',a)
     prev_frame = cur_frame
     cur_frame = next_frame
-    sum1=int(np.sum(a))
+    sum1=int(np.sum(a)) 
     next_frame = get_frame(cap)
     key = cv2.waitKey(1)
     if key == 27:
         break
-    b=frame_difference(prev_frame,cur_frame, next_frame)
-    cv2.imshow('Object Movement',b)
-    prev_frame = cur_frame
-    cur_frame = next_frame
-    sum2=int((np.sum(b)))
-    next_frame = get_frame(cap)
-    key = cv2.waitKey(1)
-    if key == 27:
-        print(a[0:1])
-        break
-##    print ("sum diff :")
-##    print (abs((sum1-sum2)))
-##    print("sum1: /n")
-##    print(sum1)
-##    print("sum2: /n")
-##    print(sum2)
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-##write your username down below the cv2.imwrite(r'C:/Users/YOURUSERNAME/....
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
-    if abs((sum1-sum2))>10000:
-        cv2.imwrite(r'C:/Users/ #### /Desktop/intruder.png', prev_frame)
+    if (sum1)>10000:
+        cv2.imwrite(os.path.join(path , raw), prev_frame)
         print("Intruder Found")
-##        cv2.imshow("intruder", prev_frame)
-##    list1.append
-    
-
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
